b96c267625fbcd56da00d031d95fa2f7d480d9c3
Hanah R <hanahratican@gmail.com>
2023-05-03 10:57:59 -0700

    Added starter code and started modifying database.js

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -1,19 +1,38 @@
 import { openDB } from 'idb';
 
 const initdb = async () =>
+// Creating a new db names 'jate' which will use version 1 of the db
   openDB('jate', 1, {
+    // add db schema if it hasn't already been initialized
     upgrade(db) {
       if (db.objectStoreNames.contains('jate')) {
         console.log('jate database already exists');
         return;
       }
+      // Create a new object store with a key path (which gives it a name of 'id') which auto increments automatically
       db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
       console.log('jate database created');
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => { 
+  console.error('putDb not implemented');
+
+  // create a connection to the database and a version we want to use
+  const contentDb = await openDB('jate', 1);
+
+  // create a transaction to write to the database
+  const tx = contentDb.transaction('jate', 'readwrite');
+
+  // create a reference to and open the object store
+  const store = tx.objectStore('jate');
+
+  // use .put() to insert or update a record in the database
+  // *********** figure out what to put in store.put() ****!
+  const request = store.put(content);
+
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
